f75838be0eedd18fb5b49c14216fdc172a30cd54
feamor84 <mail@bartekpawlowski.pl>
2017-02-14 18:28:26 +0100

    Layout modifications

--- app/src/main/java/pl/bartekpawlowski/courtcounter/MainActivity.java
+++ app/src/main/java/pl/bartekpawlowski/courtcounter/MainActivity.java
@@ -1,7 +1,10 @@
 package pl.bartekpawlowski.courtcounter;
 
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void displayForTeamA(int score) {
+        TextView containerForTeamA = (TextView) findViewById(R.id.counter_team_a);
+        containerForTeamA.setText(String.valueOf(score));
+    }
+
+    public void addThreePoints(View view) {
+        displayForTeamA(3);
+    }
+
+    public void addTwoPoints(View view) {
+        displayForTeamA(2);
+    }
+
+    public void addFreeThrowPoints(View view) {
+        displayForTeamA(1);
     }
 }
